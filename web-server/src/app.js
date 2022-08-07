a7e56d4467e7be50be25cea1f767c3cc4a389fc6
Peter Aronov <PeterAronov2@gmail.com>
2022-08-07 05:42:02 +0300

    changes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -35,7 +35,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.'
+        helpText: 'This app is used to fetch data weather about a provided location.'
     })
 })
 
